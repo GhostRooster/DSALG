906bd1f39f204a0931fa48df2a8f3328e8015f37
GhostRooster <gameformario@gmail.com>
2024-01-12 20:11:42 -0500

    arrays review

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,35 @@
 print("Hello, World!")
+
+# Arrays
+# my_array = [1, 2, 3, 4, 5]
+
+# my_array.append(6)
+# my_array.insert(0,0)
+
+# print(my_array)
+
+# my_array.remove(0)
+# my_array.pop(5)
+
+# print(my_array)
+
+# print(len(my_array))
+
+# for i in my_array:
+#     print(i)
+
+# if 4 in my_array:
+#     print("Yes")
+
+# my_array.sort()
+
+# print(my_array)
+
+# my_array.reverse()
+
+# print(my_array)
+# my_array.reverse()
+
+#############################################
+
+
